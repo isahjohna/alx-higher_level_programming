f1f13e0476c902a98af7600eeba20027050411e2
isahjohna <isahjohna@gmail.com>
2022-10-24 23:43:33 +0100

    Task 1 done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define an inherited list class MyList"""
+
+
+class MyList (list):
+    """Implements sorted printing for the buit-in list class"""
+    
+    def print_sorted(self):
+        """Print a list in sorted ascending order"""
+        print(sorted(self))
